79804e3a25e2c928c39465a4e5c0a059e853168b
Matthew North <mattnorth84@gmail.com>
2025-01-09 17:29:51 +0000

    Reworked the tabs functionality to be easier to restyle.

--- tabs.lua
+++ tabs.lua
@@ -11,97 +11,78 @@ local function tab_title(tab_info)
     return tab_info.active_pane.title
 end
 
+local function active_tab_index(tabs)
+    for i, tab in ipairs(tabs) do
+        if tab.is_active then
+            return tab.tab_index
+        end
+    end
+    return -1
+end
+
+
 wezterm.on(
     'format-tab-title',
     function(tab, tabs, panes, config, hover, max_width)
+        local function other_color(color_one, color_two)
+            return function(color)
+                if color == color_one then
+                    return color_two
+                else return color_one
+                end
+            end
+        end
+
+        local ati = active_tab_index(tabs)
         local lavender = 'rgb(180, 190, 254)'
         local blue = 'rgb(137, 180, 250)'
+        local bar_color = 'rgba(36,39,58,0.9)'
         local title = tab_title(tab)
+        local active_color = blue
+        local inactive_color = lavender
+        local tab_color = inactive_color
+        local text_color = 'rgba(255,255,255,1)'
+        local left = LEFT_TRIANGLE
+        local right = RIGHT_TRIANGLE
+        local head_text = right
+        local tail_text = left
+        local other = other_color(active_color, inactive_color)
+        local head_bg_color = tab_color
+        local head_fg_color = tab_color
+        local tail_bg_color = tab_color
+        local tail_fg_color = tab_color
         title = wezterm.truncate_right(title, max_width -2)
-        if tab.is_active and tab.tab_id == tabs[1].tab_id then
-            if #(tabs) == 1 then
-            return {
-                {Background = { Color = blue }},
-                {Foreground = { Color = '#ffffff' }},
-                {Text = " "},
-                {Background = { Color = blue }},
-                {Foreground = { Color = 'rgba(255,255,255,1)' }},
-                {Text = " " .. title .. " "},
-                {Background = { Color =  'rgba(36,39,58,0.9)'}},
-                {Foreground = { Color = blue}},
-                {Text = LEFT_TRIANGLE },
-            } else  return {
-                {Background = { Color = blue }},
-                {Foreground = { Color = '#ffffff' }},
-                {Text = " "},
-                {Background = { Color = blue }},
-                {Foreground = { Color = 'rgba(255,255,255,1)' }},
-                {Text = " " .. title .. " "},
-                {Background = { Color = lavender}},
-                {Foreground = { Color = blue}},
-                {Text = LEFT_TRIANGLE },
-            }end
-        elseif tab.is_active and tab.tab_id == tabs[#tabs].tab_id  then
-            return {
-                {Background = { Color = lavender }},
-                {Foreground = { Color = blue }},
-                {Text = RIGHT_TRIANGLE},
-                {Background = { Color = blue }},
-                {Foreground = { Color = 'rgba(255,255,255,1)' }},
-                {Text = " " .. title .. " "},
-                {Background = { Color = 'rgba(36,39,58,0.9)'}},
-                {Foreground = { Color = blue}},
-                {Text = LEFT_TRIANGLE },
-            }
-        elseif tab.is_active then
-            return {
-                {Background = { Color = lavender }},
-                {Foreground = { Color = blue }},
-                {Text = RIGHT_TRIANGLE},
-                {Background = { Color = blue }},
-                {Foreground = { Color = 'rgba(255,255,255,1)' }},
-                {Text = " " .. title .. " "},
-                {Background = { Color = lavender}},
-                {Foreground = { Color = blue}},
-                {Text = LEFT_TRIANGLE },
-            }
-        elseif not tab.is_active and tab.tab_id == tabs[#tabs].tab_id then
+        if tab.tab_index - ati  == 1 then
+            head_bg_color = tab_color
+        elseif tab.tab_index - ati == -1 then
+            tail_bg_color = other(tab_color)
+        end
+        if tab.is_active then
+            tab_color = active_color
+            tail_fg_color = tab_color
+            head_bg_color = tab_color
+            head_fg_color = tab_color
+        end
+        if tab.tab_index == 0 then
+            head_text = "  "
+            head_bg_color = tab_color
+            head_fg_color = tab_color
+        end
+        if tab.tab_index == #tabs-1 then
+            tail_bg_color = bar_color
+        end
         return {
-                {Background = { Color = blue }},
-                {Foreground = { Color = lavender }},
-                {Text = "" },
-                {Background = { Color = lavender }},
-                {Foreground = { Color = 'rgba(255,255,255,1)' }},
-                {Text = " " .. title .. " "},
-                {Background = { Color =  'rgba(36,39,58,0.9)'}},
-                {Foreground = { Color = lavender}},
-                {Text = LEFT_TRIANGLE },
-            }
-        elseif not tab.is_active and tab.tab_id == tabs[1].tab_id then
-        return {
-                {Background = { Color = lavender }},
-                {Foreground = { Color = lavender }},
-                {Text = " "},
-                {Background = { Color = lavender }},
-                {Foreground = { Color = 'rgba(255,255,255,1)' }},
-                {Text = " " .. title .. " "},
-                {Background = { Color =  lavender}},
-                {Foreground = { Color = blue}},
-                {Text = "" },
-            }
-        else return {
-                {Background = { Color = blue }},
-                {Foreground = { Color = lavender }},
-                {Text = "" },
-                {Background = { Color = lavender }},
-                {Foreground = { Color = 'rgba(255,255,255,1)' }},
-                {Text = " " .. title .. " "},
-                {Background = { Color = lavender}},
-                {Foreground = { Color = lavender}},
-                {Text = LEFT_TRIANGLE },
+                {Background = { Color = head_bg_color }},
+                {Foreground = { Color = head_fg_color }},
+                {Text = head_text},
+                {Background = { Color = tab_color }},
+                {Foreground = { Color = text_color }},
+                {Text = " " .. title .. "  "},
+                {Background = { Color = tail_bg_color}},
+                {Foreground = { Color = tail_fg_color}},
+                {Text = tail_text },
             }
         end
-    end
 )
 
 return {}
--- wezterm.lua
+++ wezterm.lua
@@ -4,9 +4,6 @@ local config  = wezterm.config_builder()
 
 local bg_opacity = 0.90
 
-local CIRCLE_CHAR = wezterm.nerdfonts.cod_circle_filled
-
-
 config.color_scheme = 'Catppuccin Macchiato'
 config.font_dirs = {'~/Library/Fonts', 'fonts'}
 config.font = wezterm.font 'JetBrains Mono'
@@ -17,10 +14,6 @@ config.window_background_image_hsb ={
     saturation = 1.0
 }
 
---[[
-the tab_bar settings below still work with fancy bar. The window frame settings
-are fancybar specific.
-]]--
 config.window_background_opacity = bg_opacity
 config.text_background_opacity = 1.0
 config.use_fancy_tab_bar = false
@@ -32,18 +25,6 @@ config.colors = {
             bg_color = 'rgba(36,39,58,0.9)',
             fg_color = '#eeeeee',
         },
-        --[[
-        active_tab = {
-            bg_color = 'rgba(138,173,244,1)',
-            fg_color = 'rgba(255,255,255,0.6)',
-            intensity = 'Normal',
-        },
-        inactive_tab = {
-            bg_color = 'rgba(180, 190, 254, 1)',
-            fg_color = '#ffffff',
-            intensity = 'Half',
-        },
-        ]]--
     },
 }
 
@@ -51,22 +32,21 @@ config.colors = {
 config.window_frame = {
     font = wezterm.font { family = 'Roboto', weight = 'Bold' },
     font_size = 16,
-    --[[
-    active_titlebar_bg = 'rgba(36,39,58,0.9)',
-    active_titlebar_fg = 'rgba(36,39,58,0.9)',
-    --active_titlebar_border_bottom = '#f74d1e',
-    inactive_titlebar_bg = 'rgba(36,39,58,0.9)',
-    inactive_titlebar_fg = 'rgba(36,39,58,0.9)',
-    --inactive_titlebar_border_bottom = '#ffffff',
-    button_fg = 'rgba(36,39,58,0.9)',
-    --button_bg = '#f74d1e',
-    ]] --
+}
+
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 25,
+    bottom = 0,
 }
 
 
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
 
-config.window_decorations = "RESIZE"
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.tab_max_width = 20
+config.tab_bar_at_bottom = true
 
 return config
